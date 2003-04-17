cb54c7f27f09f3dae40aede82e8ed1ab45681358
sits <sits>
2003-04-17 08:33:05 +0000

    Changes to handle DOS formatted topics.

--- lib/Codestriker/FileParser/Parser.pm
+++ lib/Codestriker/FileParser/Parser.pm
@@ -15,6 +15,7 @@ package Codestriker::FileParser::Parser;
 
 use strict;
 
+use FileHandle;
 use Codestriker::FileParser::CvsUnidiff;
 use Codestriker::FileParser::SubversionDiff;
 use Codestriker::FileParser::PatchUnidiff;
@@ -22,44 +23,61 @@ use Codestriker::FileParser::UnknownFormat;
 
 # Given the content-type and the file handle, try to determine what files,
 # lines, revisions and diffs have been submitted in this review.
-sub parse ($$$$) {
-    my ($type, $fh, $content_type, $repository) = @_;
+sub parse ($$$$$) {
+    my ($type, $fh, $content_type, $repository, $topicid) = @_;
 
     # Diffs found.
     my @diffs = ();
+
+    # This is a pain, but to handle diffs produced on a windoze box, which
+    # uses \r\n endings, rather than making each parser object take this
+    # into account, create a temporary file here which removes them, and
+    # that file handle is passed on to the parser objects, so they aren't
+    # the wiser.
+    my $tmp_filename = "tmpparse.$topicid";
+    open(TMP, ">$tmp_filename") ||
+	die "Unable to create temporary parse file: $!";
+    while (<$fh>) {
+	my $line = $_;
+	$line =~ s/\r\n/\n/go;
+	print TMP $line;
+    }
+    close TMP;
+    my $tmpfh = new FileHandle "$tmp_filename", "r";
+    die "Unable to open temporary parse file: $!" if (! defined $tmpfh);
 
     # If the file is plain/text, try all of the text parsers.
     if ($content_type eq "text/plain") {
 
 	# Check if it is a CVS unidiff file.
 	if ($#diffs == -1) {
-	    seek($fh, 0, 0);
+	    seek($tmpfh, 0, 0);
 	    @diffs =
-		Codestriker::FileParser::CvsUnidiff->parse($fh,
+		Codestriker::FileParser::CvsUnidiff->parse($tmpfh,
 							   $repository);
 	}
 
 	# Check if it is a Subversion diff file.
 	if ($#diffs == -1) {
-	    seek($fh, 0, 0);
+	    seek($tmpfh, 0, 0);
 	    @diffs =
-		Codestriker::FileParser::SubversionDiff->parse($fh,
+		Codestriker::FileParser::SubversionDiff->parse($tmpfh,
 							       $repository);
 	}
 
 	# Check if it is a patch unidiff file.
 	if ($#diffs == -1) {
-	    seek($fh, 0, 0);
+	    seek($tmpfh, 0, 0);
 	    @diffs =
-		Codestriker::FileParser::PatchUnidiff->parse($fh,
+		Codestriker::FileParser::PatchUnidiff->parse($tmpfh,
 							     $repository);
 	}
 
 	# Last stop-gap - the file format is unknown, treat it as a
 	# single file with filename "unknown".
 	if ($#diffs == -1) {
-	    seek($fh, 0, 0);
-	    @diffs = Codestriker::FileParser::UnknownFormat->parse($fh);
+	    seek($tmpfh, 0, 0);
+	    @diffs = Codestriker::FileParser::UnknownFormat->parse($tmpfh);
 	}
     } elsif ($content_type eq "application/gzip" ||
 	     $content_type eq "application/x-gzip") {
@@ -72,6 +90,9 @@ sub parse ($$$$) {
 
     # Restore the offset back to the start of the file again.
     seek($fh, 0, 0);
+
+    # Remove the temporary file.
+    unlink $tmp_filename;
 
     # Return the diffs found, if any.
     return @diffs;
