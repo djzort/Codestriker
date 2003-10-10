0001f8a6d2284cb2f96509b3d5f55951df8833b9
sits <sits>
2003-10-10 02:52:11 +0000

    * Fixed bug where created topics for deployments which had projects   disabled were not handled correctly.

--- lib/Codestriker.pm
+++ lib/Codestriker.pm
@@ -25,7 +25,7 @@ use vars qw ( $mailhost $use_compression $gzip $cvs $vss $bugtracker
 	      );
 
 # Version of Codestriker.
-$Codestriker::VERSION = "1.7.5";
+$Codestriker::VERSION = "1.7.6";
 
 # The maximum size of a diff file to accept.  At the moment, this is 20Mb.
 $Codestriker::DIFF_SIZE_LIMIT = 20000 * 1024;
