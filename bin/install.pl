acf99eac13480ab50d6f57d9141c0dae9e0ad5f9
sits <sits>
2006-05-22 11:39:39 +0000

    Commit to using Perl 5.8 - its the only way to get half-decent unicode support

--- bin/install.pl
+++ bin/install.pl
@@ -24,6 +24,8 @@ use strict;
 use Config;
 use lib '../lib';
 
+require 5.8.0;
+
 # Now load up the required modules.  Do this is a lazy fashion so that Perl
 # doesn't try to grab this during compile time, otherwise nasty-looking
 # error messages will appear to the user.
