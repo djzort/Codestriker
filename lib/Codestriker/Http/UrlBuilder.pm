9435ff2508ca77fa70c1bdf1f2cf3276047c8542
sits <sits>
2006-06-11 08:49:42 +0000

    I can't remember our reasoning for making our CGI & -> &amp; perhaps something to do with an html lint program complaining?
    In any case, it does seem unnecessary, and produces ugly URLs, as
    indicated by a user at:
    
    http://sourceforge.net/tracker/index.php?func=detail&aid=1460144&group_id=41136&atid=429860
    
    If there is a valid reason, let me know and we can revert it.

--- lib/Codestriker/Http/UrlBuilder.pm
+++ lib/Codestriker/Http/UrlBuilder.pm
@@ -55,13 +55,13 @@ sub view_url_extended ($$$$$$$$$$) {
 	$updated, $brmode, $fview) = @_;
     
     return ($prefix ne "" ? $prefix : $self->{query}->url()) .
-	"?topic=$topic&amp;action=view" .
+	"?topic=$topic&action=view" .
 	($updated ? "&updated=$updated" : "") .
-	((defined $tabwidth && $tabwidth ne "") ? "&amp;tabwidth=$tabwidth" : "") .
-	((defined $mode && $mode ne "") ? "&amp;mode=$mode" : "") .
-	((defined $brmode && $brmode ne "") ? "&amp;brmode=$brmode" : "") .
-	((defined $fview && $fview ne "") ? "&amp;fview=$fview" : "") .
-	((defined $email && $email ne "") ? "&amp;email=$email" : "") .
+	((defined $tabwidth && $tabwidth ne "") ? "&tabwidth=$tabwidth" : "") .
+	((defined $mode && $mode ne "") ? "&mode=$mode" : "") .
+	((defined $brmode && $brmode ne "") ? "&brmode=$brmode" : "") .
+	((defined $fview && $fview ne "") ? "&fview=$fview" : "") .
+	((defined $email && $email ne "") ? "&email=$email" : "") .
 	($line != -1 ? "#${line}" : "");
 }
 
@@ -78,14 +78,14 @@ sub view_url ($$$$$$) {
 # Create the URL for downloading the topic text.
 sub download_url ($$) {
     my ($self, $topic) = @_;
-    return $self->{query}->url() . "?action=download&amp;topic=$topic";
+    return $self->{query}->url() . "?action=download&topic=$topic";
 }
 
 # Create the URL for creating a topic.
 sub create_topic_url ($$) {
     my ($self, $obsoletes) = @_;
     return $self->{query}->url() . "?action=create" .
-	(defined $obsoletes ? "&amp;obsoletes=$obsoletes" : "");
+	(defined $obsoletes ? "&obsoletes=$obsoletes" : "");
 }	    
 
 # Create the URL for editing a topic.
@@ -93,17 +93,17 @@ sub edit_url ($$$$$$$) {
     my ($self, $filenumber, $line, $new, $topic, $context,
 	$anchor, $prefix) = @_;
     return ($prefix ne "" ? $prefix : $self->{url_prefix}) .
-	"?fn=$filenumber&amp;line=$line&amp;new=$new&amp;topic=$topic&amp;action=edit" .
-	((defined $anchor && $anchor ne "") ? "&amp;a=$anchor" : "") .
-	((defined $context && $context ne "") ? "&amp;context=$context" : "");
+	"?fn=$filenumber&line=$line&new=$new&topic=$topic&action=edit" .
+	((defined $anchor && $anchor ne "") ? "&a=$anchor" : "") .
+	((defined $context && $context ne "") ? "&context=$context" : "");
 }
 
 # Create the URL for viewing a new file.
 sub view_file_url ($$$$$$$) {
     my ($self, $topic, $filenumber, $new, $line, $mode, $parallel) = @_;
     if (!(defined $mode)) { $mode = $Codestriker::default_topic_create_mode; }
-    return $self->{url_prefix} . "?action=view_file&amp;fn=$filenumber&" .
-	"topic=$topic&new=$new&amp;mode=$mode&amp;parallel=$parallel#$filenumber|$line|$new";
+    return $self->{url_prefix} . "?action=view_file&fn=$filenumber&" .
+	"topic=$topic&new=$new&mode=$mode&parallel=$parallel#$filenumber|$line|$new";
 }
 
 # Create the URL for the search page.
@@ -148,19 +148,19 @@ sub _list_topics_url ($$$$$$$$$$$$\@\@$) {
     my $sproject = defined $project_array_ref ?
 	(join ',', @$project_array_ref) : "";
     return $self->{query}->url() . "?action=$action" .
-	($sauthor ne "" ? "&amp;sauthor=$sauthor" : "") .
-	($sreviewer ne "" ? "&amp;sreviewer=$sreviewer" : "") .
-	($scc ne "" ? "&amp;scc=$scc" : "") .
-	($sbugid ne "" ? "&amp;sbugid=$sbugid" : "") .
-	($stext ne "" ? "&amp;stext=" . CGI::escape($stext) : "") .
-	($stitle ne "" ? "&amp;stitle=$stitle" : "") .
-	($sdescription ne "" ? "&amp;sdescription=$sdescription" : "") .
-	($scomments ne "" ? "&amp;scomments=$scomments" : "") .
-	($sbody ne "" ? "&amp;sbody=$sbody" : "") .
-	($sfilename ne "" ? "&amp;sfilename=$sfilename" : "") .
-	($sstate ne "" ? "&amp;sstate=$sstate" : "") .
-	($sproject ne "" ? "&amp;sproject=$sproject" : "") .
-	(defined $content && $content ne "" ? "&amp;content=$content" : "");
+	($sauthor ne "" ? "&sauthor=$sauthor" : "") .
+	($sreviewer ne "" ? "&sreviewer=$sreviewer" : "") .
+	($scc ne "" ? "&scc=$scc" : "") .
+	($sbugid ne "" ? "&sbugid=$sbugid" : "") .
+	($stext ne "" ? "&stext=" . CGI::escape($stext) : "") .
+	($stitle ne "" ? "&stitle=$stitle" : "") .
+	($sdescription ne "" ? "&sdescription=$sdescription" : "") .
+	($scomments ne "" ? "&scomments=$scomments" : "") .
+	($sbody ne "" ? "&sbody=$sbody" : "") .
+	($sfilename ne "" ? "&sfilename=$sfilename" : "") .
+	($sstate ne "" ? "&sstate=$sstate" : "") .
+	($sproject ne "" ? "&sproject=$sproject" : "") .
+	(defined $content && $content ne "" ? "&content=$content" : "");
 }
 
 
@@ -168,7 +168,7 @@ sub _list_topics_url ($$$$$$$$$$$$\@\@$) {
 sub edit_project_url ($$) {
     my ($self, $projectid) = @_;
 
-    return $self->{query}->url() . "?action=edit_project&amp;projectid=$projectid";
+    return $self->{query}->url() . "?action=edit_project&projectid=$projectid";
 }
 
 # Construct a URL for listing all projects.
@@ -189,7 +189,7 @@ sub create_project_url ($) {
 sub view_comments_url ($$) {
     my ($self, $topic) = @_;
 
-    return $self->{query}->url() . "?action=list_comments&amp;topic=$topic";
+    return $self->{query}->url() . "?action=list_comments&topic=$topic";
 }
 
 # Create the URL for viewing the topic properties.
@@ -197,14 +197,14 @@ sub view_topic_properties_url ($$) {
     my ($self, $topic) = @_;
 
     return $self->{query}->url() .
-	"?action=view_topic_properties&amp;topic=$topic";
+	"?action=view_topic_properties&topic=$topic";
 }
 
 # Create the URL for viewing the topic metrics.
 sub view_topicinfo_url ($$) {
     my ($self, $topic) = @_;
 
-    return $self->{query}->url() . "?action=viewinfo&amp;topic=$topic";
+    return $self->{query}->url() . "?action=viewinfo&topic=$topic";
 }
 
 sub metric_report_url {
