f22e0e4db1860cdf6293920a3c75815d72da3da8
sits <sits>
2008-08-11 03:58:11 +0000

    * Created another Subversion post-commit script which is an extension   of the standard commit-email.pl script which ships with Subversion.   The email sent also includes an additional line, which is the URL to   the auto-created topic.

--- lib/Codestriker.pm
+++ lib/Codestriker.pm
@@ -36,7 +36,7 @@ use vars qw ( $mailhost $mailuser $mailpasswd $use_compression
 	      );
 
 # Version of Codestriker.
-$Codestriker::VERSION = "1.9.6";
+$Codestriker::VERSION = "1.9.7";
 
 # Default title to display on each Codestriker screen.
 $Codestriker::title = "Codestriker $Codestriker::VERSION";
